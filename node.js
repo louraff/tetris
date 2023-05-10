74a0e33ad7d9a241139be487dfd152259bd23b8b
Louise Raffray <raffraylouise@gmail.com>
2023-05-10 12:25:06 +0100

    added user input event listener and event keycodes

--- node.js
+++ node.js
@@ -2,16 +2,18 @@
 
 
 /*----- state variables -----*/
-const score;
-const level;
-const linesCleared;
-const levelUp: 5
-const startingSpeed: 500ms
+const score = 0
+const level = 1
+const linesCleared = 0
+const levelUp = 5
 
-const currentShape;
-const currentRotation;
-const nextShape;
-const ghostShape;
+const startingSpeed = 500
+const speedIncrease = 0.9
+
+const currentShape = null
+const currentRotation = null
+const nextShape = null
+const ghostShape = null
 
 const width = 10
 const height = 15
@@ -22,10 +24,40 @@ const cells = []
 const grid = document.querySelector('.grid')
 
 /*----- event listeners -----*/
-
+document.addEventListener('keydown', handleUserInput)
 
 /*----- functions -----*/
+function handleUserInput(event) {
+    console.log(event.keyCode)
 
+    const key = event.keyCode
+    const up = 38
+    const down = 40
+    const left = 37
+    const right = 39
+    const space = 32
+
+    switch (key) {
+        case left : //left
+
+            break
+        case right : //right
+
+            break
+        case down : //down
+
+            break
+        case space : //spacebar
+
+            break
+        case up : //up
+
+            break
+        default:
+            return 'Invalid Key'
+            break
+    }
+}
 
 function createGrid() {
     for (let i = 0; i < cellCount; i++) {
